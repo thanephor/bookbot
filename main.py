0bc5ae5080cdc7e7c185d6f28e1dce54ba38541a
thanephor <thanephor@proton.me>
2025-12-05 13:35:14 +0200

    feat: print formatted report

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_chars, sort_by_char_count
 
+FILEPATH = "books/frankenstein.txt"
 
 def main():
-    text = get_book_text("./books/frankenstein.txt")
-    if text:
-        num_words = get_num_words(text)
-        print(f"Found {num_words} total words")
+    text = get_book_text()
+    if not text:
+        exit(1)
+        
+    num_words = get_num_words(text)
+    num_chars = get_num_chars(text)
 
-def get_book_text(filepath):
+    sorted_chars = sort_by_char_count(num_chars)
+    print_report(num_words, sorted_chars)
+
+def get_book_text():
     try:
-        f = open(filepath)
+        f = open(FILEPATH)
     except FileNotFoundError:
-        print("no file was found in path", filepath)
+        print("no file was found in path", FILEPATH)
         return None
     else:
         with f:
             return f.read()
 
+
+def print_report(word_count, char_counts):
+    report = f"""
+============ BOOKBOT ============
+Analyzing book found at {FILEPATH}...
+----------- Word Count ----------
+Found {word_count} total words
+--------- Character Count -------"""
+    
+    print(report)
+    for entry in char_counts:
+        if entry["char"].isalpha():
+            print(f"{entry["char"]}: {entry["num"]}")
+    
+    print("============= END ===============")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,2 +1,26 @@
 def get_num_words(text):
     return len(text.split())
+
+
+def get_num_chars(text):
+    chars = {}
+    for character in text:
+        if character.isalpha():
+            character = character.lower()
+        if character in chars:
+            chars[character] += 1
+        else:
+            chars[character] = 1
+
+    return chars
+
+def sort_by_char_count(char_counts):
+    def sort_on(items):
+        return items["num"]
+    
+    res = []
+    for char, count in char_counts.items():
+        res.append({"char": char, "num" :count})
+
+    res.sort(reverse=True, key=sort_on)
+    return res
