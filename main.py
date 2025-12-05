64b2dedef397f9220267cdf68b7acff40beb34ac
thanephor <thanephor@proton.me>
2025-12-05 12:30:19 +0200

    feat: add word counter

---
+++ main.py
@@ -0,0 +1,25 @@
+
+def main():
+    text = get_book_text("./books/frankenstein.txt")
+    if text:
+        num_words = get_num_words(text)
+        print(f"Found {num_words} total words")
+
+
+def get_num_words(text):
+    return len(text.split())
+
+
+def get_book_text(filepath):
+    try:
+        f = open(filepath)
+    except FileNotFoundError:
+        print("no file was found in path", filepath)
+        return None
+    else:
+        with f:
+            return f.read()
+
+
+if __name__ == "__main__":
+    main()
